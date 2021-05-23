ed643895efb35b2bb0320ac7fc7be8d682529fd4
BruhMano <73701337+BruhMano@users.noreply.github.com>
2021-05-23 21:13:26 +0400

    finaly fixed subcategories

--- wildcherry/static/func.js
+++ wildcherry/static/func.js
@@ -13,7 +13,7 @@ function openbar() {
     document.getElementById('Sidebar').style.width = '25%';
     document.getElementById('shadow').style.zIndex = '5';
     document.getElementById('shadow').style.opacity = '50%';
-    var categories = document.querySelectorAll('.category');
+    var categories = document.querySelectorAll('.cat.category');
     for( let i = 0; i < categories.length; i++){
         categories[i].style.fontSize = '16pt';
     }
@@ -25,18 +25,24 @@ function closebar() {
     document.getElementById('shadow').style.opacity = '0';
     document.getElementById('subcategory').style.zIndex = '-5';
     document.getElementById('subcategory').style.width = '0%';
+    document.getElementById('subcategory').style.borderLeft = '0';
     var categories = document.querySelectorAll('.category');
     for( let i = 0; i < categories.length; i++){
         categories[i].style.fontSize = '0pt';
     }
 }
 
-function open_subcategoies(){
-    var category = $(this).attr('id');
-    e => span.innerText = category
+function open_subcategories(event){
+    var category = event.target.id;
+    var subcategory = document.querySelectorAll('.sub')
+    for( let i = 0; i < subcategory.length; i++){
+        subcategory[i].style.fontSize = '0pt';
+    }
+    var subcategories = document.querySelectorAll('.'+category.split(' ').join('.'),);
+    for( let i = 0; i < subcategories.length; i++){
+        subcategories[i].style.fontSize = '16pt';
+    }
     document.getElementById('subcategory').style.zIndex = '5';
     document.getElementById('subcategory').style.width = '20%';
-    document.getElementById('category').style.fontSize = '16pt';
+    document.getElementById('subcategory').style.borderLeft = 'rgb(219, 219, 219) solid 2px';
 }
-
-
